14d137211547f91f020615b5e3e4b09782be7ae9
tyrdal <tyrdal@gmx.de>
2022-02-24 10:41:47 +0100

    tune: set autoshift timeout a little higher

--- keyboards/redox_w/keymaps/tyrdal/config.h
+++ keyboards/redox_w/keymaps/tyrdal/config.h
@@ -4,7 +4,7 @@
 
 // auto shift settings
 #define AUTO_SHIFT_MODIFIERS  // enable modifiers like ctrl, alt etc to work with autoshift
-#define AUTO_SHIFT_TIMEOUT 150
+#define AUTO_SHIFT_TIMEOUT 250
 #define AUTO_SHIFT_NO_AUTO_REPEAT  // disables automatic key repetition after the timeout is exceeded
 
 #undef LEADER_TIMEOUT
