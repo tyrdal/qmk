16a2f48c2d52b560738300f1207bf8c2a4d66b49
tyrdal <tyrdal@gmx.de>
2024-11-06 08:16:34 +0100

    adjusted some timings

--- users/tyrdal/config.h
+++ users/tyrdal/config.h
@@ -4,9 +4,12 @@
 #define TAPPING_TERM 300
 
 #undef ONESHOT_TIMEOUT
-#define ONESHOT_TIMEOUT 500  // Time (in ms) before the one shot key is released
-#define ONESHOT_TAP_TOGGLE 3 // Tapping this number of times holds the key until tapped once again.
+#define ONESHOT_TIMEOUT 3000 // Time (in ms) before the one shot key is released
+#define ONESHOT_TAP_TOGGLE 2 // Tapping this number of times holds the key until tapped once again.
 
 #undef LEADER_TIMEOUT
-#define LEADER_TIMEOUT 400
+#define LEADER_TIMEOUT 1000
 #define LEADER_PER_KEY_TIMING
+
+#define CAPS_WORD_INVERT_ON_SHIFT
+#define CAPS_WORD_IDLE_TIMEOUT 3000
